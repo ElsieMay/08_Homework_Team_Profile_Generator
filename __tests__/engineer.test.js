961cea31672eb85dca57409facbf5452aba8a356
ElsieMay <elsiemaylawrie1@gmail.com>
2022-04-29 20:03:56 +1000

    added test to engineer.js

--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -6,3 +6,9 @@ test("get github string from method", () => {
 	const engineer = new Engineer("Elsie", 100, "elsie.may@gmail.com", "ElsieMay");
 	expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
 });
+
+//tests whether employee is received through getRole method
+test("get employee from role", () => {
+	const employee = new Employee("Elsie", 100, "elsie.may@gmail.com");
+	expect(employee.getRole()).toEqual("Engineer");
+});
