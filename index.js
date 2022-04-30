73b82dfd10d6ca0a9317e663155212e90405552b
ElsieMay <elsiemaylawrie1@gmail.com>
2022-04-30 10:49:10 +1000

    added variables for modules in index.js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+//required node modules
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+//employee modules
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+//HTML generated
+const generateHTML = require("./src/generateHTML");
